0964205a12bdcc28431a5b526aba576eb9c80dc0
Phi Hung Cao <caophihung8392@gmail.com>
2018-05-25 21:29:32 -0700

    add lab2

--- Week2/Triples/Triples/Program.cs
+++ Week2/Triples/Triples/Program.cs
@@ -6,20 +6,24 @@ namespace Triples
     {
         static void Main(string[] args)
         {
-            int a, b, c, i = 0, limit = 500;
+            //declare variables
+            int side1, side2, hypotenuse, limit = 500;
             Console.WriteLine("Pythagorean Triples:");
-            for (a = 1; a <= limit; a++)
+            //loop through side1
+            for (side1 = 1; side1 <= limit; side1++)
             {
-                for (b = 1 ; b <= limit; b++)
+                //loop through side2
+                for (side2 = 1 ; side2 <= limit; side2++)
                 {
-                    for (c = 1; c <= limit; c++)
+                    //loop through hypotenuse
+                    for (hypotenuse = 1; hypotenuse <= limit; hypotenuse++)
                     {
-                        if (Math.Pow(a,2) + Math.Pow(b,2) == Math.Pow(c,2))
+                        //check result if match condition then print result
+                        if (Math.Pow(side1,2) + Math.Pow(side2,2) == Math.Pow(hypotenuse,2))
                         {
-                            //   Console.WriteLine("%3d : { %3d, %3d, %3d }\n", ++i, a, b, c);
-                            Console.Write(a + " ");
-                            Console.Write(b + " ");
-                            Console.Write(c + " ");
+                            Console.Write(side1 + " ");
+                            Console.Write(side2 + " ");
+                            Console.Write(hypotenuse + " ");
                             Console.WriteLine();
                         }
                     }
