e760b2aac92658ee46845afad001f9317c8307d8
Phi Hung Cao <caophihung8392@gmail.com>
2018-07-01 22:55:49 -0700

    week 6

---
+++ Week6/Coin Tossing/Coin Tossing/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coin_Tossing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            int heads = 0;
+            int tails = 0;
+            while (!exit)
+            {
+                Console.WriteLine("1. Toss Coin \n2. Exit");
+                Console.Write("Enter choice: ");
+                int choice = Int16.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        if (flip())
+                        {
+                            heads++;
+                        }
+                        else
+                        {
+                            tails++;
+                        }
+                        Console.WriteLine("Heads: " + heads + ", Tails: " + tails);
+                        break;
+                    case 2:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("\n Invalid Input \n");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static bool flip()
+        {
+            Random rand = new Random();
+            return rand.Next(0,2) > 0 ? true : false;
+        }
+    }
+}
