546ca6fdb48ed9b7ab3c41cc52e68ffb35a8791b
CaoPhiHung <caophihung8392@gmail.com>
2018-07-07 14:36:59 -0700

    week 10

---
+++ Week10/Product Queries/Product Queries/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Product_Queries
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.productDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDBDataSet.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.productDBDataSet.Table);
+
+        }
+
+        private void sortByPriceButton_Click(object sender, EventArgs e)
+        {
+            this.tableTableAdapter.FillByPrice(this.productDBDataSet.Table);
+        }
+
+        private void unitsGreaterThan100Button_Click(object sender, EventArgs e)
+        {
+            this.tableTableAdapter.FillByUnits(this.productDBDataSet.Table);
+        }
+    }
+}
---
+++ Week8/Multiform Products2/Multiform Products/Multiform Products/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiform_Products
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.productDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDBDataSet.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.productDBDataSet.Table);
+
+        }
+
+        private void showDetailButton_Click(object sender, EventArgs e)
+        {
+            DetailForm details = new DetailForm();
+            details.ShowDialog();
+            this.tableTableAdapter.Fill(this.productDBDataSet.Table);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
